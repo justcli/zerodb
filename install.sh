d977336999b59a451c3b07c4ef1f8de8f090461c
justcli <justcli@fastmail.com>
2021-03-08 09:42:53 +0530

    updated

--- install.sh
+++ install.sh
@@ -12,21 +12,21 @@ import_path=`python3 -c "import sys;print(sys.path[-1] + '/zerodb')"`
 cli_path="/usr/local/bin/"
 
 # copy the zerodb.py as module
-mkdir $import_path
+mkdir "$import_path"
 if [ $? -ne 0 ];then
-	echo "Unable to copy files to "$import_path\
+	echo "Unable to copy files to ""$import_path"\
 			 ". Try running the script as sudo e.g. > sudo ./install.sh"
 	exit 1
 fi
 import_path=$import_path"/"
 
-cp ./zerodb.py __init__.py $import_path
+cp ./zerodb.py __init__.py "$import_path"
 if [ $? -ne 0 ];then
-	echo "Unable to copy files to "$import_path\
+	echo "Unable to copy files to ""$import_path"\
 			 ". Try running the script as sudo e.g. > sudo ./install.sh"
 	exit 1
 fi
-echo "Files copied to "$import_path":"
+echo Files copied to "$import_path":
 echo "  __init__.py"
 echo "  zerodb.py"
 
@@ -39,7 +39,7 @@ if [ $? -ne 0 ];then
 	   	 ". Try running the script as sudo e.g. > sudo ./install.sh"
 	exit 1
 fi
-echo "Files copied to "$cli_path":"
+echo "Files copied to ""$cli_path"":"
 echo "  zerodb"
 rm ./zerodb
 echo "Installation done!."
