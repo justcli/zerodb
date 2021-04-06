588f29c16e0dbb3af037ed5826579c2e7abf2119
justcli <justcli@fastmail.com>
2021-04-06 10:41:45 +0530

    updated

--- zlist.py
+++ zlist.py
@@ -6,6 +6,13 @@ import msgpack
 from io import BytesIO
 from tempfile import gettempdir
 
+"""
+Usage:
+    mylist = Zlist("mydict_unique_name", restart_time=60).load()
+    restart_time : If the app is not restarted before this time, the store
+                   of the dict variable will be deleted.
+                   If not specified, restart_time default value is 3600s
+"""
 __all__ = ['Zlist']
 
 def _save_var(self):
@@ -17,14 +24,14 @@ def _save_var(self):
 
 # Zdb-backed list class
 class Zlist(list):
-    def __init__(self, objname):
+    def __init__(self, objname, restart_time=3600):
         self._obj = []
         self._objname = objname
         self._zlistfile = gettempdir() + '/' + objname
         try:
             tm = os.stat(self._zlistfile).st_atime
             now = time.time()
-            if now - tm > 3600:
+            if now - tm > restart_time:
                 # zlist file is more than 1hr old...dump it
                 raise ValueError("Old zlist file")
             self._zlistfp = open(self._zlistfile, "rb")
